a360e6c42f1250a3e7645b59faed960573d56173
moshe832 <moshe832@gamil.com>
2024-12-19 20:34:05 +0200

    d

---
+++ multiple-agents.groovy
@@ -0,0 +1,38 @@
+def agents  = ['linux', 'Remote Machine']
+
+def generateStage(nodeLabel) {
+    return {
+        stage("Runs on ${nodeLabel}") {
+            node(nodeLabel) {
+                script {
+                    echo "Running on ${nodeLabel}"
+                    echo '************************'
+                    echo '*****BUILDING JOBS******'
+                    echo '************************'
+                //sh 'python build.py'
+                //sh 'cd ion-js && npm run prepublishOnly'
+                }
+            }
+        }
+    }
+}
+def parallelStagesMap = agents.collectEntries {
+    ["${it}" : generateStage(it)]
+}
+pipeline {
+    agent none
+    stages {
+        stage('non-parallel stage') {
+            steps {
+                echo 'This stage will be executed first.'
+            }
+        }
+        stage('parallel stage') {
+            steps {
+                script {
+                    parallel parallelStagesMap
+                }
+            }
+        }
+    }
+}
