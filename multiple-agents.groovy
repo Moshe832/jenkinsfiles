8a6570ff2378d942761eee52a7819b5988b9b4ce
moshe832 <moshe832@gamil.com>
2024-12-20 15:50:55 +0200

    s

--- multiple-agents.groovy
+++ multiple-agents.groovy
@@ -10,7 +10,7 @@ def generateStage(nodeLabel) {
                     echo '*****BUILDING JOBS******'
                     echo '************************'
                     echo 'Very Good'
-                    sh "python3 -V"
+                    sh 'python3 -V'
                 //sh 'python build.py'
                 //sh 'cd ion-js && npm run prepublishOnly'
                 }
@@ -23,11 +23,10 @@ def parallelStagesMap = agents.collectEntries {
 }
 pipeline {
     agent none
-    options { 
-    timestamps() 
-    timeout(time: 20, unit: 'SECONDS') 
+    options {
+        timestamps()
+        timeout(time: 20, unit: 'SECONDS')
     }
-   
 
     stages {
         stage('non-parallel stage') {
@@ -41,6 +40,26 @@ pipeline {
                     parallel parallelStagesMap
                 }
             }
+        }
+    }
+
+    post {
+        always {
+            sh 'echo "This will always run"'
+        }
+        success {
+            sh 'echo "This will run only if successful"'
+        }
+        failure {
+            sh 'echo "This will run only if failed"'
+        }
+        unstable {
+            sh 'echo "This will run only if the run was marked as unstable"'
+        }
+        changed {
+            sh 'echo "This will run only if the state of the Pipeline has changed"'
+            sh 'echo "For example, the Pipeline was previously failing but is now successful"'
+            sh 'echo "... or the other way around :)"'
         }
     }
 }
