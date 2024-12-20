2f0afb3ae1eedab859bd3c383a7f8338b6c19670
moshe832 <moshe832@gamil.com>
2024-12-20 16:01:31 +0200

    s

--- multiple-agents.groovy
+++ multiple-agents.groovy
@@ -40,26 +40,27 @@ pipeline {
                     parallel parallelStagesMap
                 }
             }
+
+            post {
+                always {
+                    sh 'echo "This will always run"'
+                }
+                success {
+                    sh 'echo "This will run only if successful"'
+                }
+                failure {
+                    sh 'echo "This will run only if failed"'
+                }
+                unstable {
+                    sh 'echo "This will run only if the run was marked as unstable"'
+                }
+                changed {
+                    sh 'echo "This will run only if the state of the Pipeline has changed"'
+                    sh 'echo "For example, the Pipeline was previously failing but is now successful"'
+                    sh 'echo "... or the other way around :)"'
+                }
+            }
         }
     }
 }
-    post {
-        always {
-            sh 'echo "This will always run"'
-        }
-        success {
-            sh 'echo "This will run only if successful"'
-        }
-        failure {
-            sh 'echo "This will run only if failed"'
-        }
-        unstable {
-            sh 'echo "This will run only if the run was marked as unstable"'
-        }
-        changed {
-            sh 'echo "This will run only if the state of the Pipeline has changed"'
-            sh 'echo "For example, the Pipeline was previously failing but is now successful"'
-            sh 'echo "... or the other way around :)"'
-        }
-    }
 
