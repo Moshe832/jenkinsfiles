a5c549e0b40743293b956f580f1b8e4a17321302
moshe <moshe832@gmail.com>
2025-07-19 13:51:23 +0300

    react

---
+++ react.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent { label 'windows-agent' }
+
+    tools {
+        nodejs 'NodeJS_18'  // השם כפי שהגדרת ב-Jenkins
+    }
+
+    stages {
+        stage('Clone Repository') {
+            steps {
+                git 'https://github.com/your-username/your-react-repo.git'
+            }
+        }
+
+        stage('Install Dependencies') {
+            steps {
+                bat 'npm install'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                bat 'npm run build'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                // מעתיק את תיקיית build לנתיב IIS
+                bat 'xcopy /E /Y /I build\\* C:\\inetpub\\wwwroot\\MyReactSite\\'
+            }
+        }
+    }
+}
